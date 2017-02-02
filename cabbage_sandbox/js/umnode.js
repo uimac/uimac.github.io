343c51a69db638da8d95d2371eb0d9f2f4fd7293
uimac <tori31001@gmail.com>
2017-02-03 00:59:42 +0900

    piyo

--- cabbage_sandbox/js/umnode.js
+++ cabbage_sandbox/js/umnode.js
@@ -43,12 +43,12 @@
 			verts = [],
 			normals = [];
 
-		var global = this.global_transform;
+		var global = new ummath.UMMat44d(this.global_transform);
 		var parent_global;
 		if (this.parent) {
-			parent_global = this.parent.global_transform;
+			parent_global = new ummath.UMMat44d(this.parent.global_transform);
 		} else {
-			parent_global = this.global_transform;
+			parent_global = new ummath.UMMat44d(this.global_transform);
 		}
 		ummath.um_matrix_remove_scale(global, global);
 		ummath.um_matrix_remove_scale(parent_global, parent_global);
@@ -59,8 +59,8 @@
 		global_rot.m[3][0] = global_rot.m[3][1] = global_rot.m[3][2] = 0.0;
 		var len = (end.sub(start)).length();
 		if (len <= ummath.EPSILON) { len = 1.0; }
-		var middle = (start.add(end)).scale(0.5);
 		var dir = (end.sub(start)).normalized();
+		var middle = (start.add(end)).scale(0.5).sub(new ummath.UMVec3d(dir.xyz[0], dir.xyz[1], dir.xyz[2]).scale(len * 0.2));
 		var global_y = new ummath.UMVec3d(global_rot.m[1][0], global_rot.m[1][1], global_rot.m[1][2]);
 		var global_z = new ummath.UMVec3d(global_rot.m[2][0], global_rot.m[2][1], global_rot.m[2][2]);
 
@@ -70,44 +70,44 @@
 
 		var dst_octahedron = [];
 		dst_octahedron[0] = end;
-		dst_octahedron[1] = middle.add(dir.cross(global_y).scale(0.1 * len));
-		dst_octahedron[2] = middle.add(dir.cross(global_z).scale(0.1 * len));
+		dst_octahedron[1] = middle.add(dir.cross(global_z).scale(0.1 * len));
+		dst_octahedron[2] = middle.add(dir.cross(dir.cross(global_z)).scale(0.1 * len));
 		dst_octahedron[3] = start;
-		dst_octahedron[4] = middle.add(dir.cross(global_y).scale(-0.1 * len));
-		dst_octahedron[5] = middle.add(dir.cross(global_z).scale(-0.1 * len));
-
+		dst_octahedron[4] = middle.sub(dir.cross(global_z).scale(0.1 * len));
+		dst_octahedron[5] = middle.sub(dir.cross(dir.cross(global_z)).scale(0.1 * len));
+	
 		var triangles = [
-			dst_octahedron[1],
 			dst_octahedron[0],
+			dst_octahedron[1],
+			dst_octahedron[2],
+
+			dst_octahedron[3],
+			dst_octahedron[2],
+			dst_octahedron[1],
+
+			dst_octahedron[0],
+			dst_octahedron[5],
+			dst_octahedron[1],
+
+			dst_octahedron[3],
+			dst_octahedron[1],
 			dst_octahedron[5],
 
-			dst_octahedron[1],
-			dst_octahedron[5],
-			dst_octahedron[3],
-
-			dst_octahedron[1],
-			dst_octahedron[3],
-			dst_octahedron[2],
-				
-			dst_octahedron[1],
-			dst_octahedron[2],
-			dst_octahedron[0],
-				
 			dst_octahedron[0],
 			dst_octahedron[4],
 			dst_octahedron[5],
 				
+			dst_octahedron[3],
 			dst_octahedron[5],
 			dst_octahedron[4],
-			dst_octahedron[3],
+				
+			dst_octahedron[0],
+			dst_octahedron[2],
+			dst_octahedron[4],
 				
 			dst_octahedron[3],
 			dst_octahedron[4],
-			dst_octahedron[2],
-				
-			dst_octahedron[2],
-			dst_octahedron[4],
-			dst_octahedron[0]
+			dst_octahedron[2]
 		];
 
 		for (i = 0; i < triangles.length; i = i + 1) {
@@ -118,9 +118,9 @@
 			v0 = triangles[i * 3];
 			v1 = triangles[i * 3 + 1];
 			v2 = triangles[i * 3 + 2];
-			n = v0.sub(v1).cross( v2.sub(v1) ).normalized();
+			n = v2.sub(v1).cross( v0.sub(v1) ).normalized();
 			if (n.x() === 0 && n.y() === 0 && n.z() === 0) {
-				n = v0.sub(v1).scale(10000).cross( v2.sub(v1).scale(10000) ).normalized();
+				n = v2.sub(v1).scale(10000).cross( v0.sub(v1).scale(10000) ).normalized();
 			}
 			Array.prototype.push.apply(normals, n.value());
 			Array.prototype.push.apply(normals, n.value());
@@ -133,7 +133,7 @@
 		if (this.mesh.material_list.length === 0) {
 			var meshmat = new ummaterial.UMMaterial(this.gl);
 			meshmat.set_polygon_count(verts.length / 3 / 3);
-			meshmat.set_diffuse(0.5, 0.5, 0.5, 0.5);
+			meshmat.set_diffuse(0.7, 0.7, 0.7, 1.0);
 			this.mesh.material_list.push(meshmat);
 		} else {
 			this.mesh.material_list[0].set_polygon_count(verts.length / 3 / 3);
@@ -151,9 +151,9 @@
 	UMNode.prototype.draw = function (shader, camera) {
 		if (this.mesh && this.mesh.verts.length > 0) {
 			this.gl.disable(this.gl.DEPTH_TEST);
-			this.gl.disable(this.gl.CULL_FACE);
+			//this.gl.disable(this.gl.CULL_FACE);
 			this.mesh.draw(shader, camera);
-			this.gl.enable(this.gl.CULL_FACE);
+			//this.gl.enable(this.gl.CULL_FACE);
 			this.gl.enable(this.gl.DEPTH_TEST);
 		}
 	};
