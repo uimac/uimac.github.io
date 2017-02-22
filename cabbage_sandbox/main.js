464551b67532fe70b8fe3b666d6d334b195d3b15
uimac <tori31001@gmail.com>
2017-02-23 02:03:10 +0900

    piyo

--- cabbage_sandbox/main.js
+++ cabbage_sandbox/main.js
@@ -2,6 +2,21 @@ const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 const fs = require("fs");
+const spawn = require("child_process").spawn;
+const exec = require("child_process").exec;
+
+const blender = spawn('./blender-2.76b-bepuik_tools-0.6.0-win64/blender.exe', ["-b", "reiko_bone.blend", "--python-text", "mmap"]);
+blender.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+blender.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+blender.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
 
 // Report crashes to our server.
 // electron.crashReporter.start();
@@ -31,6 +46,12 @@ app.on('ready', function() {
 	// and load the index.html of the app.
 	mainWindow = new BrowserWindow({width: 800, height: 600, title:"cabbage"});
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+	mainWindow.on('close', function () {
+		blender.kill();
+		exec("taskkill /F /T /IM blender-app.exe");
+	}); 
+
 	mainWindow.on('closed', function() {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
